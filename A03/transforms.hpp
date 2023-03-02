85ded15119bf9063cdd69efb55b62178522eaa99
carloronconi <carlo.ronconi@mail.polimi.it>
2023-03-02 16:39:47 +0100

    Improve comments for better readability

--- A03/transforms.hpp
+++ A03/transforms.hpp
@@ -7,9 +7,9 @@ void SetupMatrices(Assignment03 *A) {
 ***************/
 
 	glm::mat4 S;
-	
-	
-	// To solve the puzzle: scale 2x proportionally, centered in (2,0,-3)
+
+
+	/** To solve the puzzle: scale 2x proportionally, centered in (2,0,-3) **/
 	S = glm::mat4(1);
 
     // Transform matrices:
@@ -21,7 +21,7 @@ void SetupMatrices(Assignment03 *A) {
 
 	A->Matrix(1, R); // sets the matrix corresponding to piece 1
 
-	// To solve the puzzle: scale 2x on an axis oriented 45 degrees between the x and z axis and passing through point (1,0,0)
+    /** To solve the puzzle: scale 2x on an axis oriented 45 degrees between the x and z axis and passing through point (1,0,0) **/
 	S = glm::mat4(1);
 
     // translate to origin and rotate 45 degrees along y axis (planar rotating on xz)
@@ -33,7 +33,7 @@ void SetupMatrices(Assignment03 *A) {
 
 	A->Matrix(2, T3 * T2 * T1 * S); // sets the matrix corresponding to piece 2
 
-	// To solve the puzzle: find it yourself! Hint: compose a rotation around an arbitrary point with a translation
+    /** To solve the puzzle: find it yourself! Hint: compose a rotation around an arbitrary point with a translation **/
     // - translate z-3 and x-2 to origin
     // - rotate along y by 45 degrees
     // - translate to new position x*1 and z*2
@@ -44,15 +44,17 @@ void SetupMatrices(Assignment03 *A) {
 
 	A->Matrix(3, T2 * T1 * S); // sets the matrix corresponding to piece 3
 
-	// To solve the puzzle: rotate 60 degree around the y axis, centered in (-1,0,-2)
+    /** To solve the puzzle: rotate 60 degree around the y axis, centered in (-1,0,-2) **/
 	S = glm::mat4(1);
 	A->Matrix(4, S); // sets the matrix corresponding to piece 4
 
-	// To solve the puzzle: rotate -90 degree around an arbitrary axis passing in (-1,0,0). The x axis can be aligned to this arbitrary direction with a rotation of -45 around the y axis.	
+    /** To solve the puzzle: rotate -90 degree around an arbitrary axis passing in (-1,0,0).
+     * The x axis can be aligned to this arbitrary direction with a rotation of -45 around the y axis.**/
 	S = glm::mat4(1);
 	A->Matrix(5, S); // sets the matrix corresponding to piece 5
 
-	// To solve the puzzle: find it yourself! Hint: compose a non-proportional scaling that halves the object along one of the main axes, centered in an arbitrary point, with a translation
+    /** To solve the puzzle: find it yourself! Hint: compose a non-proportional scaling that halves the object along one of the main axes,
+     * centered in an arbitrary point, with a translation **/
 	S = glm::mat4(1);
 	A->Matrix(6, S); // sets the matrix corresponding to piece 6
 
