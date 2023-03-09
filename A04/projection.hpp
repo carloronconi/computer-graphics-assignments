6feaa17e168067333f18ea070ff6470d73e3ea2e
carloronconi <carlo.ronconi@mail.polimi.it>
2023-03-09 18:33:27 +0100

    Complete first 3 (all axonometric) projections

--- A04/projection.hpp
+++ A04/projection.hpp
@@ -2,6 +2,12 @@
 
 glm::mat4 isometricProjection(float a, float w, float n, float f);
 
+glm::mat4 dimetricProjection(float a, float w, float n, float f, float alpha);
+
+glm::mat4 trimetricProjection(float a, float w, float n, float f, float alpha, float beta);
+
+glm::mat4 parallelProjection(float a, float w, float n, float f);
+
 void SetupProjectionMatrices(Assignment04 *A, float Ar) {
 /**************
  Method Matrix(int N, glm::mat4 M) of object A,
@@ -20,11 +26,11 @@ void SetupProjectionMatrices(Assignment04 *A, float Ar) {
 	A->Matrix(1, S); // sets the matrix corresponding to piece 1
 
 	// Dimetric
-	S = glm::mat4(1);
+	S = dimetricProjection(a, w, n, f, 15.0f);
 	A->Matrix(2, S); // sets the matrix corresponding to piece 2
 
 	// Trimetric
-	S = glm::mat4(1);
+	S = trimetricProjection(a, w, n, f, 15.0f, 15.0f);
 	A->Matrix(3, S); // sets the matrix corresponding to piece 3
 
 	// Cabinet
@@ -34,16 +40,32 @@ void SetupProjectionMatrices(Assignment04 *A, float Ar) {
 }
 
 glm::mat4 isometricProjection(float a, float w, float n, float f) {
+    /*
     float deg35 = 35.26 * M_PI / 180;
     float deg45 = -45.0f * M_PI / 180;
 
-    /*
     return glm::mat4(1/w, 0, 0, 0, 0, -a/w, 0, 0, 0, 0, 1/(n-f), 0, 0, 0, n/(n-f), 1) *
     glm::mat4(1, 0, 0, 0, 0, cos(deg35), -sin(deg35), 0, 0, sin(deg35), cos(deg35), 0, 0, 0, 0, 1) *
     glm::mat4(cos(deg45), 0, -sin(deg45), 0, 0, 1, 0, 0, sin(deg45), 0, cos(deg45), 0, 0, 0, 0, 1);
     */
 
-    return glm::mat4(1/w, 0, 0, 0, 0, -a/w, 0, 0, 0, 0, 1/(n-f), 0, 0, 0, n/(n-f), 1) *
-    glm::rotate(glm::mat4(1), glm::radians(35.26f), glm::vec3(1, 0, 0)) *
-    glm::rotate(glm::mat4(1), glm::radians(-45.0f), glm::vec3(0, 1, 0));
+    return dimetricProjection(a, w, n, f, 35.26f);
+}
+
+
+glm::mat4 dimetricProjection(float a, float w, float n, float f, float alpha) {
+
+    return trimetricProjection(a, w, n, f, alpha, -45.0f);
+}
+
+glm::mat4 trimetricProjection(float a, float w, float n, float f, float alpha, float beta) {
+
+    return parallelProjection(a, w, n, f) *
+           glm::rotate(glm::mat4(1), glm::radians(alpha), glm::vec3(1, 0, 0)) *
+           glm::rotate(glm::mat4(1), glm::radians(beta), glm::vec3(0, 1, 0));
+}
+
+
+glm::mat4 parallelProjection(float a, float w, float n, float f) {
+    return glm::mat4(1/w, 0, 0, 0, 0, -a/w, 0, 0, 0, 0, 1/(n-f), 0, 0, 0, n/(n-f), 1);
 }
