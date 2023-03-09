e6c4153896002acd0a68c152860c9bf61818dad1
carloronconi <carlo.ronconi@mail.polimi.it>
2023-03-09 17:45:06 +0100

    Complete first (isometric) projection

--- A04/projection.hpp
+++ A04/projection.hpp
@@ -1,4 +1,6 @@
+#include <cmath>
 
+glm::mat4 isometricProjection(float a, float w, float n, float f);
 
 void SetupProjectionMatrices(Assignment04 *A, float Ar) {
 /**************
@@ -7,10 +9,14 @@ void SetupProjectionMatrices(Assignment04 *A, float Ar) {
 ***************/
 
 	glm::mat4 S;
-	
-	
+
+    float a = Ar; // aspect ratio
+	float w = 10.0f; // half-width
+    float n = -50.0f; // near plane
+    float f = 50.0f; // far plane
+
 	// Isometric projection
-	S = glm::mat4(1);
+	S = isometricProjection(a, w, n, f);
 	A->Matrix(1, S); // sets the matrix corresponding to piece 1
 
 	// Dimetric
@@ -25,4 +31,19 @@ void SetupProjectionMatrices(Assignment04 *A, float Ar) {
 	S = glm::mat4(1);
 	A->Matrix(4, S); // sets the matrix corresponding to piece 4
 
+}
+
+glm::mat4 isometricProjection(float a, float w, float n, float f) {
+    float deg35 = 35.26 * M_PI / 180;
+    float deg45 = -45.0f * M_PI / 180;
+
+    /*
+    return glm::mat4(1/w, 0, 0, 0, 0, -a/w, 0, 0, 0, 0, 1/(n-f), 0, 0, 0, n/(n-f), 1) *
+    glm::mat4(1, 0, 0, 0, 0, cos(deg35), -sin(deg35), 0, 0, sin(deg35), cos(deg35), 0, 0, 0, 0, 1) *
+    glm::mat4(cos(deg45), 0, -sin(deg45), 0, 0, 1, 0, 0, sin(deg45), 0, cos(deg45), 0, 0, 0, 0, 1);
+    */
+
+    return glm::mat4(1/w, 0, 0, 0, 0, -a/w, 0, 0, 0, 0, 1/(n-f), 0, 0, 0, n/(n-f), 1) *
+    glm::rotate(glm::mat4(1), glm::radians(35.26f), glm::vec3(1, 0, 0)) *
+    glm::rotate(glm::mat4(1), glm::radians(-45.0f), glm::vec3(0, 1, 0));
 }
