3f1408a03ab2d6934597e93c067545e7a47c26ac
carloronconi <carlo.ronconi@mail.polimi.it>
2023-03-27 14:18:46 +0200

    Complete assignment by adding 2 shell scripts that compile either v1 or v2 shaders

---
+++ A09/compile-v1.sh
@@ -0,0 +1,2 @@
+glslc shaders/PhongShader.vert -o shaders/PhongVert.spv
+glslc shaders/PhongShader.frag -o shaders/PhongFrag.spv
---
+++ A09/compile-v2.sh
@@ -0,0 +1,2 @@
+glslc shaders/PhongShader.v2.vert -o shaders/PhongVert.spv
+glslc shaders/PhongShader.v2.frag -o shaders/PhongFrag.spv
