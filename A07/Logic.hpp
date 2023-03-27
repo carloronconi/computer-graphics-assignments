b652455029d67c92b025ea17a6525941f72530d8
carloronconi <carlo.ronconi@mail.polimi.it>
2023-03-27 10:36:06 +0200

    INCOMPLETE: limit pitch with parameters and also use it for both character and camera and cleanup comments

--- A07/Logic.hpp
+++ A07/Logic.hpp
@@ -56,6 +56,8 @@ void GameLogic(Assignment07 *A, float Ar, glm::mat4 &ViewPrj, glm::mat4 &World) 
                                          glm::vec3(0, 1, 0)) * glm::vec4(0, 0, -1, 1));
 
     pitch += ROT_SPEED * -r.x * deltaT;
+    if (pitch > maxPitch) pitch = maxPitch;
+    if (pitch < minPitch) pitch = minPitch;
     yaw += ROT_SPEED * -r.y * deltaT;
     roll += ROT_SPEED * r.z * deltaT;
 
@@ -68,8 +70,8 @@ void GameLogic(Assignment07 *A, float Ar, glm::mat4 &ViewPrj, glm::mat4 &World) 
             glm::rotate(glm::mat4(1), pitch, glm::vec3(1, 0, 0));
             //glm::rotate(glm::mat4(1), roll, glm::vec3(0, 0, 1)); // eliminate roll character movement
 
-    float cameraPitch = glm::radians(15.0f); // TODO: camera pitch still fixed, what should it depend on?
-    auto cameraPosition = glm::vec3(0.0f); // TODO: how to get camera rotation without character rotation as in assignment requirements?
+    float cameraPitch = pitch; // TODO: camera pitch same as character and fixed rotation, verify if assignment complete
+    auto cameraPosition = glm::vec3(0.0f);
     auto cameraPositionHomogeneous = glm::vec4(0.0f);
     cameraPositionHomogeneous = World *
             glm::vec4(0.0f, camHeight + camDist * std::sin(cameraPitch), camDist * std::cos(cameraPitch), 1);
@@ -78,16 +80,7 @@ void GameLogic(Assignment07 *A, float Ar, glm::mat4 &ViewPrj, glm::mat4 &World) 
     cameraPosition.y = cameraPositionHomogeneous.y;
     cameraPosition.z = cameraPositionHomogeneous.z;
 
-    auto characterPosition = glm::vec3(0.0f);
-    /*auto characterPositionHomogeneous = glm::vec4(0.0f);
-    characterPositionHomogeneous = World * glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
-
-    characterPosition.x = characterPositionHomogeneous.x;
-    characterPosition.y = characterPositionHomogeneous.y;
-    characterPosition.z = characterPositionHomogeneous.z + camHeight;*/
-
     glm::mat4 Mv = glm::lookAt(cameraPosition, Pos, glm::vec3(0.0f, 1.0f, 0.0f));
-    //glm::mat4 Mv = glm::lookAt(glm::vec3(StartingPosition.x + 5, StartingPosition.y + 5, StartingPosition.z + 5), Pos, glm::vec3(0.0f, 1.0f, 0.0f));
     glm::mat4 Mprj = glm::perspective(FOVy, Ar, nearPlane, farPlane);
     Mprj[1][1] *= -1;
     ViewPrj = Mprj * Mv;
