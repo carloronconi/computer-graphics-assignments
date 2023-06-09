2aef467ff1b3581b7e2ab7fa0855530b26675ce6
carloronconi <carlo.ronconi@mail.polimi.it>
2023-06-09 18:18:32 +0200

    Add shader compiler script and compile shaders

---
+++ A12/compile.sh
@@ -0,0 +1,4 @@
+glslc shaders/PhongShader.frag -o shaders/PhongFrag.spv
+glslc shaders/OrenNayarShader.frag -o shaders/OrenNayarFrag.spv
+glslc shaders/BlinnNormMapShader.frag -o shaders/BlinnNormMapFrag.spv
+glslc shaders/GGXNormMapShader.frag -o shaders/GGXNormMapFrag.spv
